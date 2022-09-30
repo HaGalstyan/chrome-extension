3a0e37adb669c1fc0a265cb6169d3f9f1eed95c9
HaGalstyan <hgalstyan@krisp.ai>
2022-09-30 19:06:17 +0400

    Added tailwind css

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+import "../assets/tailwind.css";
 
 const Test = () => {
-  return <h1>TEST</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl text-green-500 ">TEST</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nihil id
+        ducimus aliquid nesciunt accusantium rerum, ab distinctio enim odio
+        natus porro, architecto tenetur. Illum veritatis quos doloribus saepe
+        alias.
+      </p>
+    </div>
+  );
 };
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(Test());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   mode: "development",
   entry: {
-    popup: "./src/popup/popup.tsx",
+    popup: path.resolve("./src/popup/popup.tsx"),
   },
+  devtool: "cheap-module-source-map",
   module: {
     rules: [
       {
@@ -14,30 +17,49 @@ module.exports = {
         test: /\.tsx$/,
         exclude: /node_modules/,
       },
+      {
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: ["tailwindcss", "autoprefixer"],
+              },
+            },
+          },
+        ],
+        test: /\.css$/i,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve("src/manifest.json"),
+          from: path.resolve("src/static"),
           to: path.resolve("dist"),
+          filter: (filepath) => filepath.includes("json"),
         },
         {
-          from: path.resolve("src/resources"),
-          to: path.resolve("dist/resources"),
+          from: path.resolve("src/static"),
+          to: path.resolve("dist/static"),
+          filter: (filepath) => !filepath.includes("json"),
         },
       ],
     }),
     new HtmlPlugin({
       title: "React.JS Webpack Extension",
       filename: "popup.html",
+      chunks: ["popup"],
     }),
   ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "index.js",
+    filename: "[name].js",
   },
 };
